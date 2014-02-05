00eb8b3b71def8792bf6b23bf03981bef165bff1
Will Lahr <williamoscarlahr@gmail.com>
2014-02-05 12:27:58 +0000

    more debug test

--- main.c
+++ main.c
@@ -42,7 +42,7 @@ int main(int argc, char **argv) {
                 
                 if(column_byte == buffer_byte)
                 {
-                    outbuff[buffer_byte]==0x01;
+                    outbuff[buffer_byte]=0x01;
                 } else {
                     outbuff[buffer_byte]=0x00;
                 }
