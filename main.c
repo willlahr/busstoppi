a6d8f3b8dddbbd6a157e0a8f3e85bcfb6c923dd1
Will Lahr <williamoscarlahr@gmail.com>
2014-02-05 16:20:12 +0000

    LINES constant was missing

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
     int n=0;
     int column = 0; // which column to light up, 0 - 25 for single board
     int count = 0;
-
+    
     uint8_t pins[] = {RPI_V2_GPIO_P1_03 , RPI_V2_GPIO_P1_05 , RPI_V2_GPIO_P1_07 ,
         RPI_V2_GPIO_P1_11, RPI_V2_GPIO_P1_13 , RPI_V2_GPIO_P1_15 , RPI_V2_GPIO_P1_16 };
     
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
     {
         bcm2835_gpio_fsel(cs_pins[pin], BCM2835_GPIO_FSEL_OUTP);
     }
-        
+    
     while(1)
     {
         for(column=0;column<COLUMN_MAX;column++)
@@ -52,10 +52,10 @@ int main(int argc, char **argv) {
             unsigned char column_byte = (column/8) ;
             unsigned char column_bit = column% 8 ;
             unsigned char buffer_byte;
-    // this block makes up some data to display. (A horizontal bar goind from left to right)
+            // this block makes up some data to display. (A horizontal bar goind from left to right)
             for(buffer_byte =0; buffer_byte< (BYTES_PER_LINE * LINES); buffer_byte++)
             {
-         //       printf("column_byte %i, buffer_byte %i, column_bit %i \n", column_byte, buffer_byte, column_bit );
+                //       printf("column_byte %i, buffer_byte %i, column_bit %i \n", column_byte, buffer_byte, column_bit );
                 if(column_byte == (buffer_byte % BYTES_PER_LINE))
                 {
                     int row  = 0;
@@ -72,34 +72,33 @@ int main(int argc, char **argv) {
                 }
                 
             }
-        
-        
-       // printf("Column %i %i %i %x %x %x %x\n", column, column_byte, column_bit, outbuff[0],outbuff[1],outbuff[2],outbuff[3]);
-        
-        
-        
+            
+            
+            // printf("Column %i %i %i %x %x %x %x\n", column, column_byte, column_bit, outbuff[0],outbuff[1],outbuff[2],outbuff[3]);
+            
+            
+            
             for(count = 0; count < 100; count++)
             {
-                    int row;
-                    for(row =0; row<ROWS; row++)
+                int row;
+                for(row =0; row<ROWS; row++)
+                {
+                    int line = 0;
+                    for(line = 0; line < 3; line++)
                     {
-                        int line = 0;
-                        for(line = 0; line < 3; line++)
-                        {
-                            
-                            // pull cs low
-                            bcm2835_gpio_clr(cs_pins[line]);
-                            bcm2835_spi_transfernb(outbuff[line] + (line * BYTES_PER_LINE), inbuff, (BYTES_PER_LINE - 1) ); // one LED line is
-                            bcm2835_gpio_set(cs_pins[line]);
-                        }
-                        // turn line on for a bit
-                        bcm2835_gpio_set(pins[row]);
-                        // delay
-                        bcm2835_delayMicroseconds(300);
-                        // turn line off
-                        bcm2835_gpio_clr(pins[row]);
-                    
+                        
+                        // pull cs low
+                        bcm2835_gpio_clr(cs_pins[line]);
+                        bcm2835_spi_transfernb(outbuff[line] + (line * BYTES_PER_LINE), inbuff, (BYTES_PER_LINE - 1) ); // one LED line is
+                        bcm2835_gpio_set(cs_pins[line]);
                     }
+                    // turn line on for a bit
+                    bcm2835_gpio_set(pins[row]);
+                    // delay
+                    bcm2835_delayMicroseconds(300);
+                    // turn line off
+                    bcm2835_gpio_clr(pins[row]);
+                    
                 }
             }
         }
