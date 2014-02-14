a26d6a910d150261ebae81d1ed61cb4698a6cffe
Will Lahr <williamoscarlahr@gmail.com>
2014-02-14 15:55:08 +0000

    put titles in between things

--- getsong.rb
+++ getsong.rb
@@ -6,6 +6,8 @@ require 'open-uri'
 while true
 
   # Now playing
+  system("writetosign \"                         \" \"  NOW PLAYING          \"  \"                         \"  \"                         \" ")
+
   doc = Nokogiri::HTML(open("http://www.last.fm/user/iamelliot"))
   if doc.css('.subjectCell.highlight').length > 0
     songcell= doc.css('.subjectCell.highlight').first
@@ -23,10 +25,13 @@ while true
 
     end
     system("writetosign #{song_info[0]} #{song_info[1]} #{song_info[2]}")
-    system('sleep 30')
+    sleep 30
 
   else
-    puts "no song playing at the moment"
+    system("writetosign \"                         \" \"       NOTHING          \"  \"                         \"  \"                         \" ")
+
+    sleep 5
+
   end
 
   # Clock
@@ -36,9 +41,9 @@ while true
 
   120.times do
   datetext = Time.now.strftime "%a %d %b        "
-  timetext = Time.now.strftime "          %H:%M:%S"
+  timetext = Time.now.strftime "%H:%M:%S"
 
-    system("writetosign \"#{datetext}\" \"#{timetext}\"  \"                         \" ")
+    system("writetosign \"#{datetext}\" \"                      \"  \"                 #{timetext}\" ")
 
     sleep 0.25
   end
@@ -53,7 +58,10 @@ while true
 
 
   sleep 20
+  system("writetosign \"       FARRINGDON        \" \"     Departures  \"  \"                         \"  \"                         \" ")
+
+
   system("traintimes")
-  sleep 20
+  sleep 30
 
 end
