383a98b0122bf2bc59af40249adbb267bc8df081
Will Lahr <williamoscarlahr@gmail.com>
2014-02-14 14:18:26 +0000

    clock and itunes now playing

--- getsong.rb
+++ getsong.rb
@@ -2,22 +2,49 @@ require 'nokogiri'
 require 'open-uri'
 
 while true
-doc = Nokogiri::HTML(open("http://www.last.fm/user/iamelliot"))
 
-songcell= doc.css('.subjectCell.highlight').first
+  # Now playing
+  doc = Nokogiri::HTML(open("http://www.last.fm/user/iamelliot"))
+  if doc.css('.subjectCell.highlight').length > 0
+    songcell= doc.css('.subjectCell.highlight').first
 
-song_info  = ["","",""]
-index = 0
-atags = songcell.css('a')
+    song_info = ["", "", ""]
+    index = 0
+    atags = songcell.css('a')
 
 
-atags.each do |atag|
-	song_info[index] << atag.text
-	song_info[index] << "                        "
-	song_info[index] = song_info[index][0..25]
-	index = index + 1
+    atags.each do |atag|
+      song_info[index] << atag.text
+      song_info[index] << "                        "
+      song_info[index] = song_info[index][0..25]
+      index = index + 1
 
-end
-system("echo #{song_info[0]} #{song_info[1]} #{song_info[2]}")
-system('sleep 5')
+    end
+    system("writetosign #{song_info[0]} #{song_info[1]} #{song_info[2]}")
+    system('sleep 30')
+
+  else
+    puts "no song playing at the moment"
+  end
+
+  # Clock
+
+
+  system("writetosign \"                         \" \"                         \"  \"                         \"  \"                         \" ")
+
+  120.times do
+  datetext = Time.now.strftime "%a %d %b        "
+  timetext = Time.now.strftime "          %H:%M:%S"
+
+    system("writetosign \"#{datetext}\" \"#{timetext}\"  \"                         \" ")
+
+    system("sleep 0.25")
+
+  end
+
+  # Tweet
+
+
+
+  system('sleep 10')
 end
